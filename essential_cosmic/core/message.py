e6c6c05784fffe9689f6448ee8164f22f9ce89cc
Tyson Denio <tysondenio@gmail.com>
2020-01-23 21:46:32 -0500

    Initial implementation of message class

---
+++ essential_cosmic/core/message.py
@@ -0,0 +1,25 @@
+import logging
+from uuid import uuid4
+from typing import Text, Dict
+
+
+class Message:
+
+    def __init__(self, offset: int, value: Text, topic):
+        self.logger = logging.getLogger('essential_cosmic.message')
+
+        self.topic = topic
+        self.offset = offset
+        self.value = value
+        self.id = self.generate_id()
+
+
+    def as_json(self) -> Dict:
+        return {"offset": self.offset, "id": self.id, "value": self.value, "topic": self.topic.id}
+
+    @staticmethod
+    def generate_id() -> Text:
+        return str(uuid4())
+
+    def __str__(self) -> Text:
+        return self.id
